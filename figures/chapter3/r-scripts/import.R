a6d9dabcd6047c8750d68237ffec21a912bc419a
WD41 <damar.wicaksono@psi.ch>
2017-08-04 18:51:01 +0200

    Factor out param_names in the common script

--- figures/chapter3/r-scripts/import.R
+++ figures/chapter3/r-scripts/import.R
@@ -3,3 +3,17 @@ library(extrafont)
 #font_install("fontcm")
 #font_import()
 loadfonts(device="win")
+
+# Use concise ID to code parameter names due to space constrained
+param_names <- c("breakP",    # break_ptb     1
+                 "fillT",     # fill_tltb     2
+                 "fillV",     # fill_vmtbm    3
+                 "pwr",       # pwr_rpwtbr    4
+                 "gridHT",    # gridHTEnh     15
+                 "iafbWHT",   # iafbWallHTC   16
+                 "dffbWHT",   # dffbWallHTC   17
+                 "dffbVIHT",  # dffbVIHTC     20
+                 "iafbIntDr", # iafbIntDrag   22
+                 "dffbIntDr", # dffbIntDrag   23
+                 "dffbWDr",   # dffbWallDrag  25
+                 "tQuench")   # tempQuench    27
