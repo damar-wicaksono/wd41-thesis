d34373d140e552955ec7fe7738b8a17128634760
WD41 <damar.wicaksono@psi.ch>
2017-08-23 15:03:34 +0200

    Revise script with a simplified variable for data path

--- figures/chapter4/r-scripts/plotRecErrorPredObs.R
+++ figures/chapter4/r-scripts/plotRecErrorPredObs.R
@@ -12,17 +12,25 @@
 # date      : Aug. 2017
 #
 # Load required libraries -----------------------------------------------------
+library(ggplot2)
 library(DiceKriging)
 
 # Global variables ------------------------------------------------------------
 # Output filename
 otpfullname <- "./figures/plotRecErrorPredObs.pdf"
 
-# Input data filename
-gp_pc_filename <- "../../../../wd41-thesis.analysis.new/trace-gp/result-gp/febaTrans216Ext-febaVars12Influential-sobol_1920_12-tc-pca-gp-powexp.Rds"
-trc_pca_filename <- "../../../../wd41-thesis.analysis.new/trace-gp/result-pc/febaTrans216Ext-febaVars12Influential-sobol_1920_12-tc-pca.Rds"
-trc_test_filename <- "../../../../wd41-thesis.analysis.new/postpro-gp-training/result-compiled/febaTrans216Ext-febaVars12Influential-lhs_1000_12_valid.Rds"
-xx_test_filename <- "../../../../wd41-thesis/simulation/gp-training/dmfiles/lhs_1000_12_valid.csv"
+# Input data filenames
+data_path_gp_pc <- "../../../../wd41-thesis.analysis.new/trace-gp/result-gp"
+gp_pc_filename <- "febaTrans216Ext-febaVars12Influential-sobol_1920_12-tc-pca-gp-powexp.Rds"
+
+data_path_trc_pca  <- "../../../../wd41-thesis.analysis.new/trace-gp/result-pc" 
+trc_pca_filename  <- "febaTrans216Ext-febaVars12Influential-sobol_1920_12-tc-pca.Rds"
+
+data_path_trc_test <- "../../../../wd41-thesis.analysis.new/postpro-gp-training/result-compiled"
+trc_test_filename <- "febaTrans216Ext-febaVars12Influential-lhs_1000_12_valid.Rds"
+
+data_path_trc_runs <- "../../../../wd41-thesis/simulation/gp-training/dmfiles"
+xx_test_filename  <- "lhs_1000_12_valid.csv"
 
 idx <- c(314, 9)    # Random selection of realization, good and bad prediction
 
@@ -31,25 +39,26 @@ fig_size <- c(5, 5)                 # width, height
 
 # Read the data ---------------------------------------------------------------
 # Read GP
-gp_pc <- readRDS(gp_pc_filename)
+gp_pc <- readRDS(paste(data_path_gp_pc, gp_pc_filename, sep = "/"))
 
 # Read Test Inputs
-xx_test <- read.csv(xx_test_filename, header = FALSE)
+xx_test <- read.csv(paste(data_path_trc_runs, xx_test_filename, sep = "/"),
+                    header = FALSE)
 names(xx_test) <- paste0("x", seq(1, 12))
 
 # Read Test Dataset
-trc_test <- readRDS(trc_test_filename)
+trc_test <- readRDS(paste(data_path_trc_test, trc_test_filename, sep = "/"))
 
 # Read PCA
-trc_train_pca <- readRDS(trc_pca_filename)
+trc_train_pca <- readRDS(paste(data_path_trc_pca, trc_pca_filename, sep = "/"))
 
 # Pre-process the data --------------------------------------------------------
 n_train <- 1920
 n_test <- 1000
 idx_outp <- 1:8
 yy_test <- matrix(0, n_test, 8 * 9999)
-for (i in 1:n_valid) yy_test[i,] <- trc_test$replicates[2:length(trc_test$time), i, idx_outp]
-rm(trc_test)
+for (i in 1:n_test) yy_test[i,] <- trc_test$replicates[2:length(trc_test$time), i, idx_outp]
+rm(trc_test) # Free memory
 
 # Shorten variable names
 yy_train_ave <- trc_train_pca$yy_train_ave  # Training samples outputs average
@@ -64,7 +73,7 @@ yy_test_scld <- sweep(yy_test_ctrd, 2, (1/yy_train_scl), `*`)
 # Project validation data to the training PC coordinate
 yy_test_scores <- yy_test_scld  %*% yy_train_loadings %*%
     diag(1/yy_train_var / (n_train - 1)) * (n_train - 1)
-rm(yy_test_scld)
+rm(yy_test_scld) # Free memory
 
 n_test <- 1000
 rec_gps_pc_error <- rep(0, n_test)
@@ -101,7 +110,6 @@ p <- ggplot(data = rec_error_df, aes(x = obs, y = pred)) +
 # Set plotting canvas
 p <- p + labs(x = "Observed Reconstruction Error (RMSE) [K]",
               y = "Predicted Reconstruction Error (RMSE) [K]")
-p <- p + scale_y_continuous(limits = y_lims)
 p <- p + theme_bw()
 p <- p + theme(axis.title.x = element_text(size = 16))
 p <- p + theme(axis.title.y = element_text(size = 16, vjust = 1.5))
