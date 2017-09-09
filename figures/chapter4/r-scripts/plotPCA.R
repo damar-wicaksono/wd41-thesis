f437e6738590bc641cec7c9a9dd5aee2e87d37fa
Damar Wicaksono <damar.wicaksono@outlook.com>
2017-09-09 10:58:30 +0200

    Update illustrative figure on PCA

--- figures/chapter4/r-scripts/plotPCA.R
+++ figures/chapter4/r-scripts/plotPCA.R
@@ -11,7 +11,7 @@ otpfullnames <- c("./figures/plotPCA_1.pdf",
                   "./figures/plotPCA_2.pdf")
 
 # Graphic Parameters
-fig_size <- c(8.5, 6.5)             # width, height
+fig_size <- c(5, 5)                 # width, height
 margin <- c(4, 5, 2.2, 1) + 0.1     # canvas margin (bot, left, top, right)
 cex_axis <- 2.5     # Axis marker size
 cex_lab  <- 3.0     # Axis label size
@@ -28,7 +28,7 @@ rr <- matrix(c(0.81, -1 * sqrt(0.81) * sqrt(0.25) * 0.85,
 
 ll <- t(chol(rr))
 
-n <- 250
+n <- 50
 zz <- matrix(ncol = 2, nrow = n)
 
 for (i in 1:n)
@@ -57,7 +57,7 @@ computeXY <- function(xx, yy, std_x, eigenvector)
 }
 
 # Data points in the original coordinate system plot --------------------------
-idx <- c(104,  62, 231, 193, 168)   # Select illustrative points
+idx <- c(23, 20, 27)         # Selected illustrative points
 pdf(otpfullnames[1], family = "CM Roman", 
     width = fig_size[1], height = fig_size[2])
 par(mfrow = c(1,1), mar = margin, las = 1,
@@ -65,7 +65,7 @@ par(mfrow = c(1,1), mar = margin, las = 1,
 
 plot(0, 0, type = "n", 
      xlim = c(mean(zz[,1]) - 1 * ceiling(3 * sqrt(0.81)), mean(zz[,1]) + ceiling(3 * sqrt(0.81))), 
-     ylim = c(mean(zz[,2]) - 1 * ceiling(3 * sqrt(0.16)), mean(zz[,2]) + ceiling(3 * sqrt(0.16))),
+     ylim = c(mean(zz[,1]) - 1 * ceiling(3 * sqrt(0.81)), mean(zz[,1]) + ceiling(3 * sqrt(0.81))),
      axes = FALSE,
      ylab = "", 
      xlab = "")
@@ -80,28 +80,24 @@ lines(computeXY(zz[,2], zz[,1], std_dev[2], zz_svd$v[,2])[[1]],
       lwd = 0.5)
 
 # Illustrative points
-points(zz[idx,1], zz[idx,2], pch = 4, cex = 2.0)
+points(zz[idx,1], zz[idx,2], pch = 20, cex = 2.0)
 
 # Put labels
 label <- data.frame(x = zz[idx,1],
                     y = zz[idx,2])
 text(label$x[1], label$y[1], 
-     labels = c(parse(text=paste("y[",idx[1],"]", sep = ""))), cex = cex_ann, pos = 4)
+     labels = c(parse(text=paste("y[",idx[1],"]", sep = ""))), cex = cex_ann, pos = 2)
 text(label$x[2], label$y[2], 
-     labels = c(parse(text=paste("y[",idx[2],"]", sep = ""))), cex = cex_ann, pos = 4)
+     labels = c(parse(text=paste("y[",idx[2],"]", sep = ""))), cex = cex_ann, pos = 3)
 text(label$x[3], label$y[3], 
-     labels = c(parse(text=paste("y[",idx[3],"]", sep = ""))), cex = cex_ann, pos = 4)
-text(label$x[4], label$y[4], 
-     labels = c(parse(text=paste("y[",idx[4],"]", sep = ""))), cex = cex_ann, pos = 4)
-text(label$x[5], label$y[5], 
-     labels = c(parse(text=paste("y[",idx[5],"]", sep = ""))), cex = cex_ann, pos = 4)
+     labels = c(parse(text=paste("y[",idx[3],"]", sep = ""))), cex = cex_ann, pos = 3)
 
-text(computeXY(zz[,1], zz[,2], std_dev[1], zz_svd$v[,1])[[1]][1]-0.1, 
-     computeXY(zz[,1], zz[,2], std_dev[1], zz_svd$v[,1])[[2]][1], 
+text(computeXY(zz[,1], zz[,2], std_dev[1], zz_svd$v[,1])[[1]][3] + 0.1, 
+     computeXY(zz[,1], zz[,2], std_dev[1], zz_svd$v[,1])[[2]][3] + 0.1, 
      labels = expression(v[1]),
      cex = 1.5)
-text(computeXY(zz[,2], zz[,1], std_dev[2], zz_svd$v[,2])[[1]][3] + 0.1, 
-     computeXY(zz[,2], zz[,1], std_dev[2], zz_svd$v[,2])[[2]][3], 
+text(computeXY(zz[,2], zz[,1], std_dev[2], zz_svd$v[,2])[[1]][1] - 0.1, 
+     computeXY(zz[,2], zz[,1], std_dev[2], zz_svd$v[,2])[[2]][1] - 0.1, 
      labels = expression(v[2]),
      cex = 1.5)
 
@@ -116,12 +112,11 @@ axis(side = 1, lwd = 1.5,
 title(xlab = expression(y[1]), mgp = c(3.25, 0, 0), cex.lab = cex_lab)
 # y-axis
 axis(side = 2, lwd = 1.5,
-     at = c(round(mean(zz[,2])) - ceiling(3 * sqrt(0.25)), 
-            round(mean(zz[,2])), 
-            round(mean(zz[,2])) + ceiling(3 * sqrt(0.25))), 
+     at = c(round(mean(zz[,1])) - ceiling(3 * sqrt(0.81)), 
+            round(mean(zz[,1])), 
+            round(mean(zz[,1])) + ceiling(3 * sqrt(0.81))), 
      mgp = c(3, cex_lab_shift, 0), 
-     tcl = tck_len, 
-     cex.axis = cex_axis)
+     tcl = tck_len, cex.axis = cex_axis)
 title(ylab = expression(y[2]), mgp = c(2.5, 0, 0), cex.lab = cex_lab)
 
 dev.off()
@@ -129,15 +124,14 @@ embed_fonts(otpfullnames[1], outfile = otpfullnames[1])
 
 
 # Data points in the new (optimal) coordinate system plot ---------------------
-
 pdf(otpfullnames[2], family = "CM Roman", 
     width = fig_size[1], height = fig_size[2])
 par(mfrow = c(1,1), mar = margin, las = 1,
     oma = c(0, 0, 0, 0), bty = "n")
 
 plot(0, 0, type = "n", 
-     xlim = c(-1 * ceiling(3 * std_dev[1]), ceiling(3 * std_dev[1])), 
-     ylim = c(-1 * ceiling(3 * std_dev[2]), ceiling(3 * std_dev[2])),
+     xlim = c(-1 * floor(3 * std_dev[1]), floor(3 * std_dev[1])), 
+     ylim = c(-1 * floor(3 * std_dev[1]), floor(3 * std_dev[1])),
      axes = FALSE,
      ylab = "", 
      xlab = "")
@@ -149,11 +143,11 @@ lines(c(-3 * sd(pc_scores[,1]), 0, 3 * sd(pc_scores[,1])), c(0, 0, 0),
 lines(c(0, 0, 0), c(-3 * sd(pc_scores[,2]), 0, 3 * sd(pc_scores[,2])), 
       lwd = 0.5)
 # Illustrative points
-points(pc_scores[idx,1], pc_scores[idx,2], pch = 4, cex = 2.0)
+points(pc_scores[idx,1], pc_scores[idx,2], pch = 20, cex = 2.0)
 label <- data.frame(x = pc_scores[idx,1],
                     y = pc_scores[idx,2])
 # Put labels
-for (i in 1:5) 
+for (i in 1:3) 
 {
     lines(c(pc_scores[idx[i],1], pc_scores[idx[i],1]), c(0, pc_scores[idx[i],2]), lwd = .5, lty = 2)
     lines(c(0, pc_scores[idx[i],1]), c(pc_scores[idx[i],2], pc_scores[idx[i],2]), lwd = .5, lty = 2)
@@ -161,27 +155,22 @@ for (i in 1:5)
 text(label$x[1], label$y[1], 
      labels = c(parse(text=paste("w[",idx[1],"]", sep = ""))), cex = cex_ann, pos = 2)
 text(label$x[2], label$y[2], 
-     labels = c(parse(text=paste("w[",idx[2],"]", sep = ""))), cex = cex_ann, pos = 3)
+     labels = c(parse(text=paste("w[",idx[2],"]", sep = ""))), cex = cex_ann, pos = 1)
 text(label$x[3], label$y[3], 
-     labels = c(parse(text=paste("w[",idx[3],"]", sep = ""))), cex = cex_ann, pos = 4)
-text(label$x[4], label$y[4], 
-     labels = c(parse(text=paste("w[",idx[4],"]", sep = ""))), cex = cex_ann, pos = 1)
-text(label$x[5], label$y[5], 
-     labels = c(parse(text=paste("w[",idx[5],"]", sep = ""))), cex = cex_ann, pos = 3)
+     labels = c(parse(text=paste("w[",idx[3],"]", sep = ""))), cex = cex_ann, pos = 1)
 
 # Set Axis, Ticks and Labels
 # x-axis
 axis(side = 1, lwd = 1.5,
-     at = c(-1 * ceiling(3 * std_dev[1]), 0, ceiling(3 * std_dev[1])), 
+     at = c(-1 * floor(3 * std_dev[1]), 0, floor(3 * std_dev[1])), 
      mgp = c(3, cex_lab_shift, 0), 
      tcl = tck_len, cex.axis = cex_axis)
 title(xlab = expression(v[1]), mgp = c(3.25, 0, 0), cex.lab = cex_lab)
 # y-axis
 axis(side = 2, lwd = 1.5,
-     at = c(-1 * ceiling(3 * std_dev[2]), 0, ceiling(3 * std_dev[2])), 
+     at = c(-1 * floor(3 * std_dev[1]), 0, floor(3 * std_dev[1])), 
      mgp = c(3, cex_lab_shift, 0), 
-     tcl = tck_len, 
-     cex.axis = cex_axis)
+     tcl = tck_len, cex.axis = cex_axis)
 title(ylab = expression(v[2]), mgp = c(2.5, 0, 0), cex.lab = cex_lab)
 
 # Save the plot
