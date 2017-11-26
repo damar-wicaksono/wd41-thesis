779aa9cca83ab24a39b7b79d1ef1ed7ba5b6a2bf
Damar Wicaksono <damar.wicaksono@gmail.com>
2017-11-26 14:57:33 +0100

    Add better comments

--- figures/chapter5/r-scripts/plotTraceUQPosteriorDP.R
+++ figures/chapter5/r-scripts/plotTraceUQPosteriorDP.R
@@ -5,6 +5,9 @@
 # author    : WD41, LRS/EPFL/PSI
 # date      : Nov. 2017
 #
+# Global variables ------------------------------------------------------------
+pa_2_bar <- 1e5
+
 # Read data -------------------------------------------------------------------
 n_sub <- 5  # Sub sample points (each point would make the file too large)
 trc_uq_prior_df <- readRDS(rds_tidy_prior_fullname)[[1]]
@@ -15,22 +18,34 @@ trc_uq_post_ind_df <- readRDS(rds_tidy_ind_fullname)[[1]][seq(1, n_t, n_sub),]
 trc_exp_df <- readRDS(rds_tidy_prior_fullname)[[2]]
 
 # Make the plot ---------------------------------------------------------------
-p <- ggplot(data = trc_uq_prior_df, aes(x = time, y = nom_run / 1e5))
+p <- ggplot(data = trc_uq_prior_df, aes(x = time, y = nom_run / pa_2_bar))
 p <- p + facet_wrap(~ax_locs, ncol = 4)
-p <- p + geom_ribbon(aes(x = time, ymin = lb_run / 1e5, ymax = ub_run / 1e5),
+# Prior uncertainty bounds
+p <- p + geom_ribbon(aes(x = time,
+                         ymin = lb_run / pa_2_bar, ymax = ub_run / pa_2_bar),
                      fill = "darkgray", alpha = 1.0)
+# Posterior, independent uncertainty bounds
 p <- p + geom_ribbon(data = trc_uq_post_ind_df,
-                     aes(x = time, ymin = lb_run / 1e5, ymax = ub_run / 1e5),
+                     aes(x = time,
+                         ymin = lb_run / pa_2_bar, ymax = ub_run / pa_2_bar),
                      fill = "lightgray", alpha = 1.0)
+# Posterior, correlated uncertainty bounds
 p <- p + geom_ribbon(data = trc_uq_post_corr_df,
-                     aes(x = time, ymin = lb_run / 1e5, ymax = ub_run / 1e5),
+                     aes(x = time,
+                         ymin = lb_run / pa_2_bar, ymax = ub_run / pa_2_bar),
                      fill = "white", alpha = 1.0)
+# Nominal run
 p <- p + geom_line()
-p <- p + geom_point(data = trc_exp_df, aes(x = time, y = exp_data / 1e5),
+# Posterior, middle run
+p <- p + geom_line(data = trc_uq_post_corr_df,
+                   aes(x = time, y = mid_run / pa_2_bar),
+                   lty = 2)
+# Experimental data
+p <- p + geom_point(data = trc_exp_df, aes(x = time, y = exp_data / pa_2_bar),
                     shape = 4, stroke = 1.0)
-p <- p + theme_bw()
 
 # Set axis labels and font size
+p <- p + theme_bw()
 p <- p + labs(x = "Time [s]",
               y = "Pressure Drop [bar]")
 p <- p + theme(strip.text.x = element_text(size = 16, face = "bold"))
